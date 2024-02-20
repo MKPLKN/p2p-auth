ec933116c15e4c21465700759ab9507a0d16530e
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-02-20 16:19:46 +0200

    Update example.js

--- example.js
+++ example.js
@@ -8,6 +8,7 @@ const masterKeyPair = generateKeyPairFromMnemonic(mnemonic)
 console.log({ masterSeed })
 
 // Derive a child seed and generate a key pair
+// On version >=2.0.0 name your child key instead of using "m/0'/1/0" deriv path.
 const childKeyPair = generateChildKeyPair(masterSeed, "m/0'/1/0")
 
 console.log('Master Key Pair:', masterKeyPair)
